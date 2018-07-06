922b0ecd2370f93d4a33688aa63828d20f4a1fa3
Roman <romanmail.by@gmail.com>
2018-07-06 07:57:11 +0300

    initial
    current changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import productsRouter from './routes/productRouter';
 import usersRouter from './routes/userRouter';
+import authRouter from './routes/authRouter';
 import cookieParsing from './middlewares/cookieParsing';
 import queryParsing from './middlewares/queryParsing';
 
@@ -13,5 +14,6 @@ app.use(express.json());
 
 app.use('/api/products', productsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
 
 export default app;
--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,17 +1,21 @@
 import { User } from '../models';
+import _ from 'lodash';
 
 //according to task app should be able to only return all users without any other functionality
 
 class UserRepository {
     constructor() {
         this.users = [
-            new User('Raman', 'Nasovich'),
-            new User('Ivan', 'Ivanov'),
-            new User('Petr', 'Petrov')
+            new User('Raman', 'Nasovich', 'roman@test.com', 'test1', '12345678'),
+            new User('Ivan', 'Ivanov', 'ivan@test.by', 'test2', '87654321'),
+            new User('Petr', 'Petrov', 'petr@test.uk', 'test3', '11111111')
         ];
     }
     getUsers(){
         return this.users;
+    }
+    authenticate(login, password){
+        return _.find(this.users, {login: login, password: password});
     }
 }
 
